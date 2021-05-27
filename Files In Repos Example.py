bd6f8d1e55d3110b1f27b2a3cb031464d37a75b1
yzdong <yzdong>
2021-05-27 23:33:47 +0000

    Yes

--- Files In Repos Example.py
+++ Files In Repos Example.py
@@ -8,3 +8,17 @@ print("cell is empty")
 # COMMAND ----------
 
 print("cell is deleted")
+
+# COMMAND ----------
+
+import os
+
+os.chdir("/Workspace/Repos/zi.dong@databricks.com/playground")
+
+# COMMAND ----------
+
+# MAGIC %sh pwd 
+
+# COMMAND ----------
+
+
